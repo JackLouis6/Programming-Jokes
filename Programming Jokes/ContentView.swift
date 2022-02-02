abbb7a1ad7280b9a54c6a03253e58b68604eb5ac
Jack Louis <jbl5925@gmail.com>
2022-02-02 10:43:09 -0600

    Finished setting up alert

--- Programming Jokes/ContentView.swift
+++ Programming Jokes/ContentView.swift
@@ -27,9 +27,23 @@ struct ContentView: View {
     }
     
     func getJokes() {
-       
-        let apiKey = "?rapidapi-key=11e76f41famsh4a8f4c04d83db94p14e066jsndbc159a476f9"
         
+        let apiKey = "?rapidapi-key=11e76f41famsh4a8f4c04d83db94p14e066jsndbc159a476f9"
+        let query = "https://dad-jokes.p.rapidapi.com/joke/type/programming\(apiKey)"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                if json["success"] == true {
+                    let contents = json["body"].arrayValue
+                    for item in contents {
+                        let setup = item["setup"].stringValue
+                        let punchline = item["punchline"].stringValue
+                        let joke = Joke(setup: setup, punchline: punchline)
+                        jokes.append(joke)
+                    }
+                }
+            }
+        }
     }
 }
 
